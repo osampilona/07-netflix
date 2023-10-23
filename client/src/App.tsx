fe9e959bba4e9daae4c895451a2b0d12071f1ebe
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2023-10-23 15:55:12 +0200

    Top navigation (navbar) (#2)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-amber-600">Hello there</h1>
+      <Navbar />
     </>
   );
 }
