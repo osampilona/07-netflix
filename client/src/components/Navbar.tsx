1fe8c2c3fc1a036b1eae1c35c698798e3d17e893
osampilona <brzicamartina@gmail.com>
2023-10-23 15:54:41 +0200

    Top navigation (navbar)

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+const tabs = [
+  "Home",
+  "Series",
+  "Films",
+  "New & Popular",
+  "My List",
+  "Browse by Language",
+];
+
+function Navbar() {
+  return (
+    <nav className="w-full fixed z-40">
+      <div className="px-16 py-6 flex items-center">
+        <img
+          className="h-16"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="logo"
+        />
+        <div className="flex gap-7 ml-8">
+          {tabs.map((tab) => (
+            <div
+              key={tab}
+              className="text-white hover:text-gray-300 cursor-pointer"
+            >
+              {tab}
+            </div>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
