7afc1598a331a88c198cdbcfc7927af31a73449c
osampilona <brzicamartina@gmail.com>
2023-10-29 16:37:33 +0100

    Browse page data in movie cards and list

--- client/src/components/MovieList.tsx
+++ client/src/components/MovieList.tsx
@@ -1,13 +1,15 @@
+import { Movie } from "../types";
 import MovieCard from "./MovieCard";
 
-function MovieList() {
+function MovieList({ movies }: { movies: Movie[] }) {
   return (
     <div className="px-12 mt-4 space-y-8">
       <div>
         <p className="text-black text-2xl font-semibold mb-4">Popular Shows</p>
         <div className="flex flex-wrap gap-2 justify-between">
-          <MovieCard />
-          <MovieCard />
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       </div>
     </div>
