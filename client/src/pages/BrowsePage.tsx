19b55a6ed6908e2869abff679caa83f818b2a5dd
osampilona <brzicamartina@gmail.com>
2023-10-31 12:23:32 +0100

    Paggination on scroll

--- client/src/pages/BrowsePage.tsx
+++ client/src/pages/BrowsePage.tsx
@@ -2,19 +2,39 @@ import Billboard from "../components/Billboard";
 import Navbar from "../components/Navbar";
 import MovieList from "../components/MovieList";
 import useMoviesList from "../hooks/useMoviesList";
+import { useState, useRef, useCallback } from "react";
+import LoadingCards from "../components/LoadingCards";
 
 function BrowsePage() {
-  const { data, loading, error } = useMoviesList();
-  console.log({ data, loading, error });
+  const [offset, setOffset] = useState(0);
+  const { data, loading, error } = useMoviesList(offset);
+  const observer = useRef<null | IntersectionObserver>(null);
+
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (loading) return;
+
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setOffset(offset + 12);
+          console.log("intersecting");
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [loading]
+  );
 
   return (
     <div>
       <Navbar />
       <Billboard />
       <div className="pb-r">
-        {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
-        {data && <MovieList movies={data} />}
+        {data && <MovieList movies={data} lastElementRef={lastElementRef} />}
+        {loading ? <LoadingCards /> : null}
       </div>
     </div>
   );
